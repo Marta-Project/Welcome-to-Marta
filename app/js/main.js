24516508316063e99beddeef072d628f242a99f7
robert fine <robertfine4631@icloud.com>
2015-07-01 19:42:21 -0400

    added basic styling and notes within scss

--- app/js/main.js
+++ app/js/main.js
@@ -9,22 +9,22 @@
     $routeProvider
 
     .when('/', {
-      templateUrl: 'js/dashboard/dashboard.tpl.html',
+      templateUrl: '/dashboard/dashboard.tpl.html',
       controller: 'DashBoard'
     })
 
     .when('/about', {
-      templateUrl: 'js/about/about.tpl.html',
+      templateUrl: '/about/about.tpl.html',
       controller: 'About'
     })
 
     .when('/contact', {
-      templateUrl: 'js/contact/contact.tpl.html',
+      templateUrl: '/contact/contact.tpl.html',
       controller: 'Contact'
     })
 
     .when('/trains', {
-      templateUrl: 'js/trains/trains.tpl.html',
+      templateUrl: '/trains/trains.tpl.html',
       controller:   'Trains'
      });
     
