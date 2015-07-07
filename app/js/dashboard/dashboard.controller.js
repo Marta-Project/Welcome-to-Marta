d8ae5844a34b09a58816d7d533659c1fd380a8e4
robert fine <robertfine4631@icloud.com>
2015-07-07 15:48:40 -0400

    cookies can be deleted with the logout button

--- app/js/dashboard/dashboard.controller.js
+++ app/js/dashboard/dashboard.controller.js
@@ -4,13 +4,26 @@
 
   angular.module('marta')
 
-  .controller('Dashboard', ['$scope', '$http', 'PARSE', '$cookies',
+  .controller('Dashboard', ['$scope', '$http', 'PARSE', '$cookies', '$location',
 
-    function ($scope, $http, PARSE, $cookies ){
+    function ($scope, $http, PARSE, $cookies, $location ){
 
       $scope.userName = $cookies.get('username');
 
+      $scope.sesh = $cookies.get('sessionToken');
 
+
+      $scope.logout = function () {
+
+        var removeSessionToken = function () {$cookies.remove('sessionToken');};
+        var removeUserName = function () {$cookies.remove('username');};
+
+        removeSessionToken();
+        removeUserName();
+
+
+
+      };
 
 
     }
