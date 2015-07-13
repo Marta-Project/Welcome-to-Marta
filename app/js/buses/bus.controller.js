270999e3bd443689fbcaedf2db10490604f89e6f
chris bustamante <chrisbusta@hotmsil.com>
2015-07-13 11:40:24 -0400

    changes

--- app/js/buses/bus.controller.js
+++ app/js/buses/bus.controller.js
@@ -8,12 +8,27 @@
 
     function ($scope, $http){
 
+    $http.get('https://jsonp.afeld.me/?url=http://developer.itsmarta.com/BRDRestService/RestBusRealTimeService/GetAllBus')
+
+
+}
+var busEndpoint = 'http://developer.itsmarta.com/BRDRestService/RestBusRealTimeService/GetAllBus';
+
+
+      $.getJSON( busEndpoint, function( data ) {
+        var items = [];
+        $.each( data, function( name, item ) {
+          items.push( "<li id='" + name + "'>" + item + "</li>" );
+        });
+
+        $( "<ul/>", {
+          "class": "my-new-list",
+          html: items.join( "" )
+        }).appendTo( "#traintest" );
+      });
 
 
 
-
-
-    }
     ]);
 
 }());
--- app/js/trains/trains.controller.js
+++ app/js/trains/trains.controller.js
@@ -8,7 +8,20 @@
 
     function ($scope, $http){
 
+    // var trainEndpoint = 'http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apiKey=72f588d5-9412-4a89-8d83-0ef58e91cac1';
 
+
+      $.getJSON( trainEndpoint, function( data ) {
+        var items = [];
+        $.each( data, function( name, item ) {
+          items.push( "<li id='" + name + "'>" + item + "</li>" );
+        });
+
+        $( "<ul/>", {
+          "class": "my-new-list",
+          html: items.join( "" )
+        }).appendTo( "body" );
+      });
 
 
 
