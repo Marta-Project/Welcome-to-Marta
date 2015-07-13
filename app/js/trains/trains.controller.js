bb3cdb82d4e9d7fc3349f6c3f1dc2b5655a23e2d
robert fine <robertfine4631@icloud.com>
2015-07-13 12:19:51 -0400

    all train objects will show up on page

--- app/js/trains/trains.controller.js
+++ app/js/trains/trains.controller.js
@@ -10,30 +10,21 @@
 
 
 
-      $http.get('https://jsonp.afeld.me/?url=http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apiKey=72f588d5-9412-4a89-8d83-0ef58e91cac1');
+     var trainData = $http.get('https://jsonp.afeld.me/?url=http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apiKey=72f588d5-9412-4a89-8d83-0ef58e91cac1').success( function (data){
+        console.log(data);
+        $scope.trains = data;
+
+      })
+     .error (function (err){
+      console.error(err);
+     });
+
+     // console.log(trainData);
 
 
-
-      var trainEndpoint = 'https://jsonp.afeld.me/?url=http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apiKey=72f588d5-9412-4a89-8d83-0ef58e91cac1';
-
+     $scope.trains2 = trainData;
 
 
-
-      // $.getJSON( trainEndpoint, function( data ) {
-      //   var items = [];
-      //   $.each( data, function( name, item ) {
-      //     items.push( "<li id='" + name + "'>" + item + "</li>" );
-      //   });
-
-      //   $( "<ul/>", {
-      //     "class": "my-new-list",
-      //     html: items.join( "" )
-      //   }).appendTo( "#traintest" );
-      // });
-
-
-
-        // console.log();
 
     }
     ]);
