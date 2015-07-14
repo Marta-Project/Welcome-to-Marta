4bb0be68c3636d52d10f47df720309e82a9c98d2
robert fine <robertfine4631@icloud.com>
2015-07-14 14:40:08 -0400

    added refresh button, but still need it to clear the current data

--- app/js/trains/trains.controller.js
+++ app/js/trains/trains.controller.js
@@ -39,6 +39,34 @@
     console.error(err);
    });
 
+   // Refresh Button
+    $scope.refresh = function () {
+        $http
+    .get('https://jsonp.afeld.me/?url=http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apiKey=72f588d5-9412-4a89-8d83-0ef58e91cac1')
+    .success( function (data) {
+      console.log(data);
+      $scope.trains = data;
+      angular.forEach($scope.trains, function (train){
+        if( train.DIRECTION === "N") {
+          $scope.northTrains.push(train);
+        }
+        else if( train.DIRECTION === "S"){
+          $scope.southTrains.push(train);
+        }
+        // else if( train.DIRECTION === "N" && train.LINE === "GOLD") {
+        //   $scope.northTrainsGold.push(train);
+        // }
+
+
+      });
+      console.log($scope.northTrainsRed);
+      console.log($scope.northTrainsGold);
+      console.log($scope.southTrains);
+    })
+   .error (function (err){
+    console.error(err);
+   });
+    };
     }
     ]);
 
