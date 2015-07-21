7f2663e48282f96daade4534535768e093b3ac06
robert fine <robertfine4631@icloud.com>
2015-07-21 16:02:53 -0400

    populated list with current stations

--- app/js/dashboard/dashboard.controller.js
+++ app/js/dashboard/dashboard.controller.js
@@ -30,6 +30,34 @@
         $scope.favoriteLocation = !$scope.favoriteLocation;
       };
 
+      // Array of all stations to choose a favorite from
+      $scope.stations = [{'Station' : 'North Springs'},
+        {'Station' : 'Sandy Springs'},
+        {'Station' : 'Dunwoody'},
+        {'Station' : 'Medical Center'},
+        {'Station' : 'Buckhead'},
+        {'Station' : 'Doraville'},
+        {'Station' : 'Chamblee'},
+        {'Station' : 'Brookehaven/Oglethorpe'},
+        {'Station' : 'Lenox'},
+        {'Station' : 'Lindbergh Center'},
+        {'Station' : 'Arts Center'},
+        {'Station' : 'Midtown'},
+        {'Station' : 'North Avenue'},
+        {'Station' : 'Civic Center'},
+        {'Station' : 'Peachtree Center'},
+        {'Station' : 'Five Points'},
+        {'Station' : 'Garnett'},
+        {'Station' : 'West End'},
+        {'Station' : 'Oakland City'},
+        {'Station' : 'Lakewood/Ft. McPherson'},
+        {'Station' : 'East Point'},
+        {'Station' : 'College Park'},
+        {'Station' : 'Airport'}];
+
+
+
+
 
     }
 
