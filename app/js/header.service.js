1096336df8618db4c33a2377c75b122904ab35fa
robert fine <robertfine4631@icloud.com>
2015-07-21 21:18:35 -0400

    setting up service for headers

---
+++ app/js/header.service.js
@@ -0,0 +1,16 @@
+;(function (){
+  'use strict';
+
+  angular.module('marta')
+
+  .service('HeaderService', ['$location', 'PARSE',
+
+    function ($location, PARSE) {
+
+
+
+
+    }
+
+    ]);
+}());
